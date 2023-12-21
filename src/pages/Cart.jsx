6d9634868d244cf14f780e4cff35869507b8670d
HaJin Lim <dlagkwls119@naver.com>
2023-12-21 20:19:58 +0900

    ver1.3

--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -1,8 +1,8 @@
-import { useDispatch, useSelector } from 'react-redux';
-import cssStyle from '../css/Cart.module.css';
-import { changeName, changeRate } from '../store/user';
-import { plusCount, minusCount, delItem } from '../store/cartList';
-import { useNavigate } from 'react-router-dom';
+import { useDispatch, useSelector } from "react-redux";
+import cssStyle from "../css/Cart.module.css";
+import { changeName, changeRate } from "../store/user";
+import { plusCount, minusCount, delItem } from "../store/cartList";
+import { useNavigate } from "react-router-dom";
 export default function Cart() {
   let user = useSelector((a) => a.user);
   let cartList = useSelector((a) => a.cartList);
@@ -10,12 +10,11 @@ export default function Cart() {
   let navigate = useNavigate();
   let iconTrash = (
     <svg
-      xmlns="http://www.w3.org/2000/svg"
-      height="16"
-      width="14"
-      viewBox="0 0 448 512"
-    >
-      <path d="M135.2 17.7C140.6 6.8 151.7 0 163.8 0H284.2c12.1 0 23.2 6.8 28.6 17.7L320 32h96c17.7 0 32 14.3 32 32s-14.3 32-32 32H32C14.3 96 0 81.7 0 64S14.3 32 32 32h96l7.2-14.3zM32 128H416V448c0 35.3-28.7 64-64 64H96c-35.3 0-64-28.7-64-64V128zm96 64c-8.8 0-16 7.2-16 16V432c0 8.8 7.2 16 16 16s16-7.2 16-16V208c0-8.8-7.2-16-16-16zm96 0c-8.8 0-16 7.2-16 16V432c0 8.8 7.2 16 16 16s16-7.2 16-16V208c0-8.8-7.2-16-16-16zm96 0c-8.8 0-16 7.2-16 16V432c0 8.8 7.2 16 16 16s16-7.2 16-16V208c0-8.8-7.2-16-16-16z" />
+      xmlns='http://www.w3.org/2000/svg'
+      height='16'
+      width='14'
+      viewBox='0 0 448 512'>
+      <path d='M135.2 17.7C140.6 6.8 151.7 0 163.8 0H284.2c12.1 0 23.2 6.8 28.6 17.7L320 32h96c17.7 0 32 14.3 32 32s-14.3 32-32 32H32C14.3 96 0 81.7 0 64S14.3 32 32 32h96l7.2-14.3zM32 128H416V448c0 35.3-28.7 64-64 64H96c-35.3 0-64-28.7-64-64V128zm96 64c-8.8 0-16 7.2-16 16V432c0 8.8 7.2 16 16 16s16-7.2 16-16V208c0-8.8-7.2-16-16-16zm96 0c-8.8 0-16 7.2-16 16V432c0 8.8 7.2 16 16 16s16-7.2 16-16V208c0-8.8-7.2-16-16-16zm96 0c-8.8 0-16 7.2-16 16V432c0 8.8 7.2 16 16 16s16-7.2 16-16V208c0-8.8-7.2-16-16-16z' />
     </svg>
   );
   return (
@@ -32,8 +31,7 @@ export default function Cart() {
           onClick={() => {
             dispatch(changeName());
             dispatch(changeRate(10));
-          }}
-        >
+          }}>
           회원정보 변경
         </button>
       </p>
@@ -45,9 +43,11 @@ export default function Cart() {
               className={cssStyle.img}
               onClick={() => {
                 navigate(`/detail/${item._id}`);
-              }}
-            >
-              <img src={`/img/${item.img}`} alt={item.title} />
+              }}>
+              <img
+                src={`${process.env.PUBLIC_URL}/img/${item.img}`}
+                alt={item.title}
+              />
             </div>
             <div className={cssStyle.title}>{item.title}</div>
             <div className={cssStyle.num}>
@@ -60,8 +60,7 @@ export default function Cart() {
                 <button
                   onClick={() => {
                     dispatch(minusCount(item._id));
-                  }}
-                >
+                  }}>
                   -
                 </button>
               )}
@@ -69,8 +68,7 @@ export default function Cart() {
               <button
                 onClick={() => {
                   dispatch(plusCount(item._id));
-                }}
-              >
+                }}>
                 +
               </button>
             </div>
@@ -80,8 +78,7 @@ export default function Cart() {
             <button
               onClick={() => {
                 dispatch(delItem(item._id));
-              }}
-            >
+              }}>
               {iconTrash}
             </button>
           </li>
